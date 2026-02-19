dc5ea291422aa511f1b171f1320329c7b1933757
Stephen Derenski <stephen.derenski@marathon.health>
2026-02-19 16:03:57 -0700

    changes to the version

--- .github/scripts/checkVersionConflict.js
+++ .github/scripts/checkVersionConflict.js
@@ -8,30 +8,14 @@ const ancestorParts = ancestorVersion.split(".").map(Number);
 const mainParts = mainVersion.split(".").map(Number);
 const currentParts = currentPrVersion.split(".").map(Number);
 
-let conflict;
-console.log(ancestorParts, mainParts, currentParts);
-
 const ancestorCompare = compareVersions(ancestorParts, mainParts, false);
 const prCompare = compareVersions(currentParts, mainParts, true);
 
 const equalEachOther =
   ancestorVersion === mainVersion && currentPrVersion === mainVersion;
 
-console.log("equalEach other", equalEachOther);
-console.log(`ancestorCompare: ${ancestorCompare} - prCompare: ${prCompare}`);
-console.log(`(ancestorCompare && prCompare): ${ancestorCompare && prCompare}`);
-console.log(
-  `(ancestorCompare && prCompare) || equalEachOther: ${
-    (ancestorCompare && prCompare) || equalEachOther
-  }`,
-);
-
 if ((ancestorCompare && prCompare) || equalEachOther) {
   setOutput("conflict", "true");
-  conflict = true;
 } else {
   setOutput("conflict", "false");
-  conflict = false;
 }
-
-console.log(`conflict result ${conflict}`);
