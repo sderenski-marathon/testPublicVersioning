ed53f70daf99ccd3c9392e4cd6c87a9fd12575ad
Stephen Derenski <stephen.derenski@marathon.health>
2026-02-19 09:34:14 -0700

    feat: changing header

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Github Actions Testing</h1>
+      <h1>Github Actions Testing V2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
