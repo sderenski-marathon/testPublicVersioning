6b13bc57593f13ad8c666178a592a2dd2c4db76b
Stephen Derenski <stephen.derenski@marathon.health>
2026-02-19 15:46:59 -0700

    testing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// App root
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
